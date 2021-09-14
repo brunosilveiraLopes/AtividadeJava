ff290ec14cd4034ba4f47bb5a59b3b7607f57387
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:09:29 -0300

    Update Transportadora.java

--- Transportadora.java
+++ Transportadora.java
@@ -1,8 +1,44 @@
 package calculadora;
 
 public class Transportadora {
-	public String endereco;
-	public String telefone;
-	public String rastreamento;
-	public String nomeCompleto;
+	private String endereco;
+	private String telefone;
+	private String rastreamento;
+	private String nome;
+	
+	public Transportadora(String endereco, String telefone, String rastreamento, String nome) {
+		this.endereco = endereco;
+		this.nome = nome;
+		this.rastreamento = rastreamento;
+		this.telefone = telefone;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public String getRastreamento() {
+		return rastreamento;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public void setRastreamento(String rastreamento) {
+		this.rastreamento = rastreamento;
+	}
+	@Override
+	public String toString() {
+		return String.format("Nome: %\tRastreamento: %\tTelefone: %\tEndereco: %",this.nome,this.rastreamento,this.telefone,this.endereco);
+		
+	}
+	
+	
 }
