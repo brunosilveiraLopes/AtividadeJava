cc94b97dfb7adf8f32524e9369fb3ec1d896d3c7
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:08:38 -0300

    Update Clientes.java

--- Clientes.java
+++ Clientes.java
@@ -1,8 +1,40 @@
 package calculadora;
 
 public class Clientes {
-		public String nomeCompleto;
-		public String numero;
-		public String telefone;
-		public String cpf;
+	private String nome;
+	private String numero;
+	private String telefone;
+	private String cpf;
+	
+	public Clientes(String nome, String telefone, String cpf, String numero) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		this.numero = numero;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Nome: %\tTelefone: %\tEndereco: %sNumero: %s",this.nome,this.telefone,this.cpf);
+	}
+	
+	
+}
