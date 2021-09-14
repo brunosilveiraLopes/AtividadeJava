524681b1fbb50bee32241f101b9c69f7a1e80343
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:10:29 -0300

    Update Fornecedores.java

--- Fornecedores.java
+++ Fornecedores.java
@@ -1,8 +1,44 @@
 package calculadora;
 
 public class Fornecedores {
-	public String filial;
-	public String endereco;
-	public String nomeCompleto;
-	public String telefone;
+	private String filial;
+	private String endereco;
+	private String nome;
+	private String telefone;
+	
+	public Fornecedores(String filial, String endereco, String nome, String telefone) {
+		this.filial = filial;
+		this.endereco = endereco;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+	
+	public String getFilial() {
+		return filial;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Filial: %\tNome: %\tTelefone: %\tEndereco: %",this.filial,this.nome,this.telefone,this.endereco);
+		
+	}
+	
 }
