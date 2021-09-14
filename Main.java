d5b8f1ce329ee8f580a540de882ff8e2add5ec27
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:07:55 -0300

    Update Main.java
    Loja de varejo (adicionado Getters/Setter and toString

--- Main.java
+++ Main.java
@@ -1,44 +1,22 @@
 package calculadora;
 
 public class Main {
+	
+		public static void main(String[] args) {
+			//Criando os objetos
+			Clientes Clie = new Clientes("Bruno Silveira Lopes","224","980113334","888-222-333-11");
+			Estoque Esto = new Estoque("Bruno Varejo","Hebert richard","Nike-adidas puma edition");
+			Fornecedores Forn = new Fornecedores("Varejo Hebert","Fatimas rua 777","Hebert Richard","980321332");
+			Produtos Prod = new Produtos("Camisa da nike com adidas","2.799$","Loja oficial Nike");
+			Transportadora Trans = new Transportadora("EndereÃ§o Sedex","21 875523123","89K382AL2 19","Sedex");
+			Vendas Ven = new Vendas("833.562.312.71","Silveira Bruno","Sistema da nike","11 3244012","276mil");
+			Vendedores Vende = new Vendedores("323-414-666-49","Rodrigo farah","884721402","21 987753402");
 
-	public static void main(String[] args) {
-		//Criando os objetos
-		Clientes Clie = new Clientes();
-		Estoque Esto = new Estoque();
-		Fornecedores Forn = new Fornecedores();
-		Produtos Prod = new Produtos();
-		Transportadora Trans = new Transportadora();
-		Vendas Ven = new Vendas();
-		Vendedores Vende = new Vendedores();
-		
-		//Popular os dados
-		Clie.nomeCompleto = "Bruno Silveira Lopes";
-		Clie.cpf = "888-888-888-33";
-		
-		Esto.nomeDoProduto = "Camiseta da Nike(Premium)";
-		Esto.filial = "Nike-OFICIAL";
-		
-		Forn.endereco = "Rua taramram 255";
-		Forn.filial = "Nikezinha";
-		
-		Prod.produto = "Air Jordan Edition Travis Scott";
-		Prod.preço = " $4.750 dollares";
-		
-		Trans.rastreamento = "K3782K9182 90";
-		Trans.endereco = "Correio SEDEX";
-		
-		Ven.sistema = "Sistema da Empresa Nike";
-		Ven.nomeCompleto = "Silveira lopes Bruno";
-		
-		Vende.cpf = "777-666-444-20";
-		Vende.nomeCompleto = "Silveira Lopes Bruno";
-		
-		//Imprimir os dados
-		System.out.printf("O senhor %s do cpf %s gostaria de saber se tem em estoque o produto %s da filial %s. depois ele gostaria de comprar"
-				+ "o produto %s pelo preço de %s, e perguntou sobre a transportada e o endereco dos fornecedores %s, %s, %s e o rastreamento dela %s para saber por onde anda o produto,"
-				+ "o Vendedor do Senhor %s foi o %s do cpf %s, é só ver a venda no sistema %s e o nome dele %s.", Clie.nomeCompleto,Clie.cpf,
-				Esto.nomeDoProduto,Esto.filial,Prod.produto,Prod.preço,Forn.endereco,Forn.filial,Trans.endereco,Trans.rastreamento,Clie.nomeCompleto,Vende.nomeCompleto,Vende.cpf,Ven.sistema,Ven.nomeCompleto);
+			//Imprimir os dados
+			System.out.printf(
+					
+					"O Cliente %s Realizou uma compra de %s no valor de %s. O produto estarÃ¡ disponivel no %s, e sera entregue pela transportadora %s",
+				Clie.getNome(),Prod.getProduto(),Prod.getPreÃ§o(),Trans.getRastreamento(), Trans.getNome());
+			
 	}
-
 }
