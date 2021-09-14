d985aaee5001f453f590a7fb9c5d70927b2eabba
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:10:46 -0300

    Update Vendedores.java

--- Vendedores.java
+++ Vendedores.java
@@ -1,8 +1,38 @@
 package calculadora;
 
 public class Vendedores {
-	public String nomeCompleto;
-	public String cpf;
-	public String rg;
-	public String telefone;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String telefone;
+	
+	public Vendedores(String nome, String cpf, String rg, String telefone) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.rg = rg;
+		this.telefone = telefone;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Nome: %\tCpf: %\tRG: %\tTelefone: %",this.nome,this.cpf,this.rg,this.telefone);
+		
+	}
+	
 }
