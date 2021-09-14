843bd051fbafd645773534f274aa59a0789f0e09
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:09:03 -0300

    Update Estoque.java

--- Estoque.java
+++ Estoque.java
@@ -1,7 +1,40 @@
 package calculadora;
 
 public class Estoque {
-	public String filial;
-	public String nomeCompleto;
-	public String nomeDoProduto;
+	private String filial;
+	private String nome;
+	private String nomeDoProduto;
+	
+	public Estoque(String filial, String nome, String nomeDoProduto) {
+		this.filial = filial;
+		this.nome = nome;
+		this.nomeDoProduto = nomeDoProduto;
+	}
+	
+	public String getFilial() {
+		return filial;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Filial: %\tNome: %\tNomeDoProduto: %s",this.filial,this.nome,this.nomeDoProduto);
+	}
+	
+	
 }
+
