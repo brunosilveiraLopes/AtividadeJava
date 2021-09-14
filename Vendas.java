1e00addd4972ff2623e47456ed54af9761945449
brunosilveiraLopes <89555108+brunosilveiraLopes@users.noreply.github.com>
2021-09-14 01:10:09 -0300

    Update Vendas.java

--- Vendas.java
+++ Vendas.java
@@ -1,9 +1,49 @@
 package calculadora;
 
 public class Vendas {
-	public String valores;
-	public String sistema;
-	public String nomeCompleto;
-	public String cpf;
-	public String telefone;
+	private String valores;
+	private String sistema;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	
+	public Vendas(String valores, String sistema, String nome, String cpf, String telefone) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.sistema = sistema;
+		this.telefone = telefone;
+		this.valores = valores;
+	}
+	
+	public String getValores() {
+		return valores;
+	}
+	public String getSistema() {
+		return sistema;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setValores(String valores) {
+		this.valores = valores;
+	}
+	public void setSistema(String sistema) {
+		this.sistema = sistema;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Valores: %\tSistema: %\tNome: %\tCpf: %",this.valores,this.sistema,this.nome,this.cpf);
+		
+	}
+	
 }
